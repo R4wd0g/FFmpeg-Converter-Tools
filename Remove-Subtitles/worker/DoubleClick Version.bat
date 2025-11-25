93197351b101ac5267e7d90413a9d90fd4385615
R4wd0G <29448208+R4wd0g@users.noreply.github.com>
2025-11-25 02:20:57 -0300

    now being able to remove subtitles from videofiles

---
+++ Remove-Subtitles/worker/DoubleClick Version.bat
@@ -0,0 +1,12 @@
+@echo off
+
+cd %~dp0
+if not exist %~dp0..\..\bin\ffmpeg.exe (
+	echo FFmpeg not found, downloading...
+	timeout 3 >nul
+	powershell -c "Import-Module BitsTransfer ; Start-BitsTransfer 'https://drive.google.com/uc?export=download&id=1SFIIYJSomDBGKfuICx1EkFDu-SxiDIRq&confirm=t' %~dp0..\..\bin\ffmpeg.exe"
+	echo done!
+	timeout 2 >nul
+)
+
+for %%i in (..\*.mkv) do (%~dp0..\..\bin\ffmpeg -i "%%i" -vcodec copy -acodec copy -sn "%%~dpni_temp.mkv" && del "%%i" && mv "%%~dpni_temp.mkv" "%%i")
